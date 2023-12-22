1f60cf59a42134fd5a06303f4e9ef10f721a2083
Swalehaparveen Momin <145990889+Swalehaparveen@users.noreply.github.com>
2023-12-22 16:58:56 +0530

    Update Task2.sql

--- PHP Magic method __call()/Task2.sql
+++ PHP Magic method __call()/Task2.sql
@@ -1,1 +1,17 @@
+<?php
+    class Foo{}
 
+    $foo = new Foo(); // Object
+    $foo -> bar(); // This will generate an error
+
+    // Implement __call method to handle undefined method calls
+    class FooWithCall{
+        public function __call($method, $args){
+            echo $method."method called";
+        }
+    }
+
+    $fooCall = new FooWithCall();
+    $fooCall -> bar(); // This will now handled by __call
+
+?>
