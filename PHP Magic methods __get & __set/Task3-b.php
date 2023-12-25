9172bed2007750705d369e0415e3b8ef0947d69f
Swalehaparveen Momin <145990889+Swalehaparveen@users.noreply.github.com>
2023-12-25 16:23:49 +0530

    Update Task3-b.php

--- PHP Magic methods __get & __set/Task3-b.php
+++ PHP Magic methods __get & __set/Task3-b.php
@@ -1,1 +1,26 @@
+<?php
+    // use case of __get() and  __set()
+    class User{
+        private $userData = [];
+        public function __get($name){
+            return $this->userData[$name] ?? null;
+        }
 
+        public function __set($name, $value){
+            $this->userData[$name]=$value;
+        }
+    }
+
+    $user = new User();
+
+    // Set user properties dynamically using __set()
+    $user->name = 'Merry';
+    $user->age = 23;
+    $user->email = 'merry@gmail.com';
+
+    // Retrieve user propertues dynamically using __get()
+    echo "Name:".$user->name."\n";
+    echo "Age:".$user->age."\n";
+    echo "Email:".$user->email."\n";
+    echo "City:".$user->city."\n";
+?>
