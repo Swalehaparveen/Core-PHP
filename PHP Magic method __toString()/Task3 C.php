944947d871de3cd7ff04ddc49f1e471d59db982f
Swalehaparveen Momin <145990889+Swalehaparveen@users.noreply.github.com>
2023-12-25 16:22:05 +0530

    Update Task3 C.php

--- PHP Magic method __toString()/Task3 C.php
+++ PHP Magic method __toString()/Task3 C.php
@@ -1,1 +1,20 @@
+<?php
+    // The usecase of __toString() PHP Magic method.
 
+    class Product{
+        private $name;
+        private $price;
+
+        public function __construct($name, $price){
+            $this->name = $name;
+            $this->price = $price;
+        }
+        public function __toString(){
+            return "Product: {$this->name}, Price: {$this ->price}";
+        }
+    }
+
+    $product = new Product("Phone", 25000.00);
+
+    echo $product; // output: Product: Phone, 25000.00
+?>
