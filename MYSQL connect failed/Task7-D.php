2179e07eae27098f040664e72a30bffb624f2279
Swalehaparveen Momin <145990889+Swalehaparveen@users.noreply.github.com>
2024-01-01 21:00:39 +0530

    Update Task7-D.php

--- MYSQL connect failed/Task7-D.php
+++ MYSQL connect failed/Task7-D.php
@@ -1,1 +1,19 @@
+<?php
+    /**
+     * 4. MYSQL connection failed
+     */
 
+     //MYSQL connect failed
+     $servername = "localhost";
+     $username = "swaleha";
+     $password = "root";
+     $database = "test";
+
+     //Create connection
+     $conn = new mysqli($servername, $username, $password, $database);
+
+     //check connection
+     if($conn ->connect_error){
+        die("Connection failed: ". $conn->connect_error);
+     }
+?>
