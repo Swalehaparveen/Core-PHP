240dcad9fcc8efd343177d5c9743e7069a26409d
Swalehaparveen Momin <145990889+Swalehaparveen@users.noreply.github.com>
2024-01-01 20:58:45 +0530

    Update Task7-C.php

--- Undefined index/Task7-C.php
+++ Undefined index/Task7-C.php
@@ -1,1 +1,19 @@
 
+<?php
+    /**
+     * 3. Undefined index
+     */
+
+     // Create an array without defining an index
+     $array = array(
+        "name" => "John",
+        "age" => 35,
+        // "email" key is missing
+     );
+
+     //Trying to access an undefined index 'email'
+     echo $array["name"];
+     echo $array["age"];
+     echo $array["email"];
+
+?>
