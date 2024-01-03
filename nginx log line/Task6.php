95e713fc7500f40c6b0477feff686eb0cbf148cd
Swalehaparveen Momin <145990889+Swalehaparveen@users.noreply.github.com>
2024-01-03 13:14:20 +0530

    Update Task6.php

--- nginx log line/Task6.php
+++ nginx log line/Task6.php
@@ -1,1 +1,39 @@
+<?php
+$logFilePath = '/var/log/nginx/access.log.1'; // Update this with the correct path to your NGINX access log file
 
+function parseNginxLogLine($logLine) {
+    $logParts = explode(' ', $logLine);
+
+    if (count($logParts) >= 12) {
+      //  $userAgentStart = strpos($logLine, '"');
+        //$userAgentEnd = strrpos($logLine, '"');
+        //$userAgent = substr($logLine, $userAgentStart + 1, $userAgentEnd - $userAgentStart - 1);
+
+        $parsedLog = [
+            'ip' => $logParts[0],
+            'timestamp' => $logParts[3] . $logParts[4],
+            'method' => $logParts[5],
+            'path' => $logParts[6].' '.$logParts[7], 
+            'status_code' => $logParts[8],
+            'size' => $logParts[9],
+            'user_agent' => implode(' ', array_slice($logParts, 11)) // $userAgent User agent
+        ];
+        return $parsedLog;
+    }
+    return null; // Return null for lines that don't match the expected format
+}
+
+$logLines = file($logFilePath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+$parsedLogs = [];
+foreach ($logLines as $logLine) {
+    $parsedLog = parseNginxLogLine($logLine);
+    if ($parsedLog !== null) {
+        $parsedLogs[] = $parsedLog;
+    }
+}
+
+$jsonLogs = json_encode($parsedLogs, JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES);
+
+echo $jsonLogs; // Outputting JSON representation of parsed NGINX access log lines
+?>
